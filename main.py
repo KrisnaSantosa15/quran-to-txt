66d8a73f444f1c7bd431e8c62abd54888bb8c878
krisnasantosa <krisnasantosa@smknegeri1garut.sch.id>
2023-11-03 17:19:30 +0700

    Add License and command to run the program

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+# Author: Krisna Santosa
+# Description: This script will retrieve all verses from the Quran API and save them to text files.
+# The text files will be saved in the text folder.
+# run this script with python3 main.py
+
 import requests
 import os
 
